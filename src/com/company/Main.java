2f2e02cbd315c66882134ce87fc290893c6a6773
Rajesh <72iarud@gmail.com>
2017-06-01 23:39:12 +0930

    Added proper params downloadProject function and moved most of function in try catch block. Removed default classlist function. Class list must be provided manually in as commandline args

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,52 +23,24 @@ public class Main {
     //must be fed in values
     public static void main(String[] args) throws Exception{
 
-        String USERNAME = "";
-        String PASSWORD = "";
-        String ASSIGNMENT = "";
-
-
-        if(args[0].equals("test")){
-            USERNAME = "a1675993";
-            PASSWORD = "rajeshdurai27$$$";
-
-        }else{
-            //get username
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter username: ");
-            USERNAME = sc.nextLine();
-            //get password, but don't display on screen
-            /*Console c;
-            char[] passwordChar;
-            //get the console to hide input
-            if(((c = System.console()) != null) && (passwordChar = c.readPassword("%s", "Password: ")) != null){
-                PASSWORD = passwordChar.toString();
-            }else{
-                System.out.println("No password entered! Exiting...");
-                System.exit(0);
-            }*/
-            System.out.println("Enter password: ");
-            PASSWORD = sc.nextLine();
-
-            System.out.println("Enter assignment php url: ");
-            ASSIGNMENT = sc.nextLine();
+        if(args.length != 1){
+            System.out.println("Please input the classlist!");
+            System.exit(1);
         }
 
-        //run in loop or comment out and run individually
-        /*for(int exam = 1; exam < 4; exam++){
-            for(int question = 1; question < 4; question++){
-                ASSIGNMENT = "pracexam" + exam + "p" + question;
-                downloadProject(USERNAME, PASSWORD, ASSIGNMENT);
-            }
-        }*/
+        String USERNAME = "a1675993";
+        String PASSWORD = "rajeshdurai27$$$";
+        String ASSIGNMENT = "";
+        String COURSE = "adsa";
+        String SEMESTER = "s2";
+        String YEAR = "2015";
 
 
-
-        downloadProject(USERNAME, PASSWORD, "pracexam1p2");
+        downloadProject(USERNAME, PASSWORD, YEAR, COURSE, SEMESTER, ASSIGNMENT, args[0]);
 
     }
 
-    public static  void downloadProject(String USERNAME, String PASSWORD, String ASSIGNMENT) throws Exception{
+    public static  void downloadProject(String USERNAME, String PASSWORD, String YEAR, String COURSE, String SEMESTER, String ASSIGNMENT, String classListPath) throws Exception{
         WebAuthAndRequest wr = new WebAuthAndRequest();
 
         //traversing the login page
@@ -84,75 +56,74 @@ public class Main {
 
         //navigate the php
         //select the course details
-        wr.getPage("https://cs.adelaide.edu.au/services/websubmission/?sub_year=2015&sub_period=s2&sub_course=pssd&sub_assign=" + ASSIGNMENT);
+        wr.getPage("https://cs.adelaide.edu.au/services/websubmission/?sub_year=" + YEAR + "&sub_period=" + SEMESTER + "&sub_course=" + COURSE + "&sub_assign=" + ASSIGNMENT);
         //go to the classlist page
         wr.getPage("https://cs.adelaide.edu.au/services/websubmission/?menu=Classlist");
 
 
+        /*
+        *
+        * WARNING GET CLASS LIST MANUALLY!
+        *
+        * */
         //get each student so we can make requests for their feedback pages
         ClassListParser clp = new ClassListParser();
 
         //create meta directory
         MainHelper.createDirectory("./" + ASSIGNMENT + "/meta", "meta");
-        //TODO: get the class list online
-        String csvPath = "./" + ASSIGNMENT + "/meta/students.csv";
 
         ArrayList<String> classList; // stores the id numbers of students
 
         try{
-            classList = clp.readAndParseClassList(csvPath);
-        }catch(FileNotFoundException f){
-            //default to csv file we stored
-            System.out.println("No file at " + csvPath + ".\n" + "Using default file...\n");
-            classList = clp.readAndParseClassList("./students.csv");
-        }
+            classList = clp.readAndParseClassList(classListPath);
 
-        for (String s : classList){
-            System.out.println(s);
-        }
+            //create tarGets directory
+            MainHelper.createDirectory("./" + ASSIGNMENT + "/tarGets", "tarGets");
 
-        //create tarGets directory
-        MainHelper.createDirectory("./" + ASSIGNMENT + "/tarGets", "tarGets");
-
-        //now make request for each student
-        for (String id : classList){
-            //this gets to their feedback page
-            String studentPage = wr.getPage("https://cs.adelaide.edu.au/services/websubmission/?menu=" +
-                    "View%20Feedback&sub_output_select=feedback&sub_alt_user=" + id);
-            //then select the svn revision we need for that user
-            Document d = Jsoup.parse(studentPage);
+            //now make request for each student
+            for (String id : classList){
+                //this gets to their feedback page
+                String studentPage = wr.getPage("https://cs.adelaide.edu.au/services/websubmission/?menu=" +
+                        "View%20Feedback&sub_output_select=feedback&sub_alt_user=" + id);
+                //then select the svn revision we need for that user
+                Document d = Jsoup.parse(studentPage);
 
             /*
             select the revisions by targeting like
             * <a href="?sub_output_select=revision-279"> Aug 20 10:36 r279(100) </a>
             * */
-            Elements revisions = d.select("a[href*=revision]");
+                Elements revisions = d.select("a[href*=revision]");
 
-            for (Element e : revisions){
-                System.out.println(e.text());
-                System.out.println(e.attr("href") + "\n");
+                for (Element e : revisions){
+                    System.out.println(e.text());
+                    System.out.println(e.attr("href") + "\n");
+                }
+
+
+                //select the latest revision if it exists
+                if(revisions.size() > 0){
+                    System.out.println("Downloading for " + id + "...");
+                    String revMark = MainHelper.getMark(revisions.get(0).text());
+
+                    studentPage = wr.getPage("https://cs.adelaide.edu.au/services/websubmission/" + revisions.get(0).attr("href"));
+
+                    //finally download the source tar file
+                    URL dlLink = new URL("https://cs.adelaide.edu.au/services/websubmission/download.php?download_file=exported.tgz");
+                    ReadableByteChannel rbc = Channels.newChannel(dlLink.openStream());
+                    FileOutputStream fos = new FileOutputStream("./" + ASSIGNMENT + "/tarGets/" + id + "_" + revMark + ".tgz");
+                    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                }else{
+                    // TODO: 14/05/17 keep track of non-submissions in a file?
+                    System.out.println("No submission for " + id);
+                }
+
             }
-
-
-
-            //select the latest revision if it exists
-            if(revisions.size() > 0){
-                System.out.println("Downloading for " + id + "...");
-                String revMark = MainHelper.getMark(revisions.get(0).text());
-
-                studentPage = wr.getPage("https://cs.adelaide.edu.au/services/websubmission/" + revisions.get(0).attr("href"));
-
-                //finally download the source tar file
-                URL dlLink = new URL("https://cs.adelaide.edu.au/services/websubmission/download.php?download_file=exported.tgz");
-                ReadableByteChannel rbc = Channels.newChannel(dlLink.openStream());
-                FileOutputStream fos = new FileOutputStream("./" + ASSIGNMENT + "/tarGets/" + id + "_" + revMark + ".tgz");
-                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-            }else{
-                // TODO: 14/05/17 keep track of non-submissions in a file?
-                System.out.println("No submission for " + id);
-            }
-
+        }catch(FileNotFoundException f){
+            //default to csv file we stored
+           f.printStackTrace();
+           System.exit(1);
         }
+
     }
 
     private static class MainHelper{
